454ed3ab59dd9c46ee258cb10c183ddf2a90fa5d
Ahmet Yanık <ahmetyaniklar@gmail.com>
2024-04-25 01:06:59 +0300

    final_update

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.starting_page),
-    path("posts", views.posts),
-    path("posts/<slug>",views.post_detail)
+    path("", views.starting_page,name="starting-page"),
+    path("posts", views.posts,name="posts-page"),
+    path("posts/<slug:slug>",views.post_detail
+         ,name="post-detail-page")
 ]
